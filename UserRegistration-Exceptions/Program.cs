d278123a26eda6a5e6404d088e3b9e62ddc64f38
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-23 19:09:58 +0530

    Add project files.

---
+++ UserRegistration-Exceptions/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserRegistration_Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("****** Welcome To Exceptions Handling in User registration ! ******");
+            Console.WriteLine("===================================================================");
+        }
+    }
+}
