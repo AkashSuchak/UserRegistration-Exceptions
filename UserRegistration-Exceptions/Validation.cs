324c7895e3bdb0afbe642f38e3d036f46cba232c
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-08-24 17:35:35 +0530

    feat: Added: Invalid LastName throw an Exception

--- UserRegistration-Exceptions/Validation.cs
+++ UserRegistration-Exceptions/Validation.cs
@@ -7,11 +7,18 @@ namespace UserRegistration_Exceptions
 {
     public class Validation
     {
+        //Const for REGEX
         const string REGEX_NAME = "^[A-Z]{1}[A-Za-z]{2,}";
+
         public void FirstName(string firstName)
         {                       
             if (Regex.IsMatch(firstName, REGEX_NAME) == false)         
                 throw new ExceptionHandling(ExceptionHandling.ExceptionsCustomMessage.INVALID_INPUT, " : Name start with Capital and has Minimum 3 characters");         
+        }
+        public void LastName(string lastName)
+        {
+            if (Regex.IsMatch(lastName, REGEX_NAME) == false)
+                throw new ExceptionHandling(ExceptionHandling.ExceptionsCustomMessage.INVALID_INPUT, " : Name start with Capital and has Minimum 3 characters");
         }
     }
 }
--- UserRegistrationMSTest/UnitTest1.cs
+++ UserRegistrationMSTest/UnitTest1.cs
@@ -6,12 +6,27 @@ namespace UserRegistrationMSTest
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// FirstName UC1
+        /// </summary>
+        /// <param name="firstName"></param>
         [TestMethod]
         [DataRow("akash")]
-        public void Given_InvalidInput_ThrowExceptionCustomMessage_InvalidInput(string firstName)
+        public void Given_InvalidInput_FirstName_ThrowExceptionCustomMessage_InvalidInput(string firstName)
         {
             Validation validation = new Validation();                        
             Assert.ThrowsException<ExceptionHandling>(() => validation.FirstName(firstName));
+        }
+        /// <summary>
+        /// Last Name UC2
+        /// </summary>
+        /// <param name="lastName"></param>
+        [TestMethod]
+        [DataRow("suchak")]
+        public void Given_InvalidInput_LastName_ThrowExceptionCustomMessage_InvalidInput(string lastName)
+        {
+            Validation validation = new Validation();
+            Assert.ThrowsException<ExceptionHandling>(() => validation.LastName(lastName));
         }
     }
 }
